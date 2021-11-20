baea8ea9b57d70b3c2eeafc4765020c0bdcbdb75
yeonjin seong <syj3405@naver.com>
2021-11-20 14:36:55 +0900

    학생 데이터에 기능 (함수) 추가   - 연도를 넣으면 -> 내 출생년도와 계산해서 나이로 결과를 주는 기능

--- app/src/main/java/com/neppplus/listview_20211120/datas/StudentData.kt
+++ app/src/main/java/com/neppplus/listview_20211120/datas/StudentData.kt
@@ -6,4 +6,14 @@ class StudentData(
     val name: String,
     val birthYear: Int,
     val address: String ) {
+
+//    학생데이터의 기능 추가 -> 년도를 넣으면 나이로 계산  (함수 제작)
+
+    fun getAgeByYear( year : Int ) : Int{
+
+        val age = year - this.birthYear + 1
+        return  age
+
+    }
+
 }
